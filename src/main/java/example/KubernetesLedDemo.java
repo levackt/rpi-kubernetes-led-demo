faad5a1455da55aac8c93c6effdfff19e9fa4b80
Taariq Levack <taariql@gmail.com>
2016-11-13 06:46:00 +0200

    Initial commit

--- src/main/java/example/KubernetesLedDemo.java
+++ src/main/java/example/KubernetesLedDemo.java
@@ -1,10 +1,9 @@
-package com.ctjc.app;
+package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.ctjc")
 @SpringBootApplication
 public class KubernetesLedDemo {
 
